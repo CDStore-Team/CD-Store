2e5962d048a75c49f0f24c86f1f6264bd1ac1dc0
sn <rock097abc@gmail.com>
2026-04-09 00:47:59 +0300

    Amends to host on Github pages

--- Project/vite.config.ts
+++ Project/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	base: '/CD-Store/',
   server: {
     host: "::",
     port: 8080,
